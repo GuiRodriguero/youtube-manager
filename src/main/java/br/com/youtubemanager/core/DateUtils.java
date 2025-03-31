62695e613ad45ad02b4b33604a1ca7f1c228935d
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-03-31 00:15:09 -0300

    Creating DateUtils

--- src/main/java/br/com/youtubemanager/core/DateUtils.java
+++ src/main/java/br/com/youtubemanager/core/DateUtils.java
@@ -1,4 +1,24 @@
 package br.com.youtubemanager.core;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class DateUtils {
+
+    public static String formatDate(String date) {
+        if (date == null || date.isEmpty()) {
+            return "";
+        }
+        try {
+            DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'");
+            DateTimeFormatter outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            LocalDateTime dateTime = LocalDateTime.parse(date, inputFormatter);
+            return dateTime.format(outputFormatter);
+        } catch (DateTimeParseException e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
 }
