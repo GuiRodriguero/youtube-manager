8c5e89d5fbc950b0225ebe13ab354e9fad5b5f8d
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-04-06 00:13:20 -0300

    Creating ChannelNotFoundException

---
+++ src/main/java/br/com/youtubemanager/channel/ChannelNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.youtubemanager.channel;
+
+public class ChannelNotFoundException extends RuntimeException {
+    public ChannelNotFoundException(String message) {
+        super(message);
+    }
+}
