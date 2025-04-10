4e74ca52652bf52199022afbe16792e0d989807b
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-04-10 00:47:27 -0300

    Refactoring ChannelService to reduce code

--- src/main/java/br/com/youtubemanager/channel/web/ChannelService.java
+++ src/main/java/br/com/youtubemanager/channel/web/ChannelService.java
@@ -3,10 +3,7 @@ package br.com.youtubemanager.channel.web;
 import br.com.youtubemanager.channel.ChannelDTO;
 import br.com.youtubemanager.channel.ChannelNotFoundException;
 import com.google.api.services.youtube.YouTube;
-import com.google.api.services.youtube.model.ChannelListResponse;
-import com.google.api.services.youtube.model.ChannelSnippet;
-import com.google.api.services.youtube.model.ChannelStatistics;
-import com.google.api.services.youtube.model.Thumbnail;
+import com.google.api.services.youtube.model.*;
 import org.apache.commons.collections4.CollectionUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -25,34 +22,22 @@ class ChannelService {
 
 	public ChannelDTO findOne(String channelName) {
 		try {
-			YouTube.Channels.List request = getYouTubeService().channels()
+			List<Channel> channels = getYouTubeService().channels()
 				.list("snippet,contentDetails,statistics")
 				.setForUsername(channelName)
-				.setKey(apiKey);
-
-			ChannelListResponse response = request.execute();
-			List<com.google.api.services.youtube.model.Channel> channels = response.getItems();
+				.setKey(apiKey)
+				.execute()
+				.getItems();
 
 			if (CollectionUtils.isEmpty(channels)) {
 				throw new ChannelNotFoundException("Channel not found");
 			}
 
-			return convertToMyChannel(channels.getFirst());
+			return ChannelDTO.of(channels.getFirst());
 		}
 		catch (IOException e) {
 			throw new RuntimeException("Failed to get channel information", e);
 		}
-	}
-
-	private ChannelDTO convertToMyChannel(com.google.api.services.youtube.model.Channel youtubeChannel) {
-		ChannelSnippet snippet = youtubeChannel.getSnippet();
-		ChannelStatistics statistics = youtubeChannel.getStatistics();
-		Thumbnail thumbnail = snippet.getThumbnails().getDefault();
-
-		return ChannelDTO.of(snippet.getTitle(), snippet.getCustomUrl(), snippet.getDescription(),
-				snippet.getPublishedAt().toString(), thumbnail.getUrl(), snippet.getLocalized().getDescription(),
-				snippet.getCountry(), formatNumber(statistics.getViewCount()),
-				formatNumber(statistics.getSubscriberCount()), formatNumber(statistics.getVideoCount()));
 	}
 
 }
