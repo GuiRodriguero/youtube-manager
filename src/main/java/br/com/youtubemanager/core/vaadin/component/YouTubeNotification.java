d16fc6cf602eb0a480a8571631538a85503c146a
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-04-12 11:41:14 -0300

    Enhance notification with close button and improved layout
    Updated the notification to include a close button for better user control. Adjusted the layout to align message and button, and changed the default position to TOP_END for improved visibility.

--- src/main/java/br/com/youtubemanager/core/vaadin/component/YouTubeNotification.java
+++ src/main/java/br/com/youtubemanager/core/vaadin/component/YouTubeNotification.java
@@ -1,15 +1,35 @@
 package br.com.youtubemanager.core.vaadin.component;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.notification.NotificationVariant;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.theme.lumo.LumoIcon;
 
 import static com.vaadin.flow.component.notification.Notification.Position.MIDDLE;
+import static com.vaadin.flow.component.notification.Notification.Position.TOP_END;
 
 public class YouTubeNotification {
 
-	public static void show(String errorMessage, NotificationVariant theme) {
-		Notification errorNotification = Notification.show(errorMessage, 3000, MIDDLE);
-		errorNotification.addThemeVariants(theme);
+	public static void show(String message, NotificationVariant theme) {
+		Notification notification = Notification.show(message, 3000, TOP_END);
+
+		Div notificationMessage = new Div(message);
+
+		Button closeButton = new Button(new Icon("lumo", "cross"));
+		closeButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY_INLINE);
+		closeButton.setAriaLabel("Close");
+		closeButton.addClickListener(event -> notification.close());
+
+		HorizontalLayout layout = new HorizontalLayout(notificationMessage, closeButton);
+		layout.setAlignItems(FlexComponent.Alignment.CENTER);
+
+		notification.add(layout);
+		notification.addThemeVariants(theme);
 	}
 
 }
