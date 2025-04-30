a6851840106dc1343c2be151ea0fd206a818ae85
GuiRodriguero <gui.rodriguero@hotmail.com>
2025-04-30 19:42:42 -0300

    Creating YouTubeManagerException

---
+++ src/main/java/br/com/youtubemanager/core/YouTubeManagerException.java
@@ -0,0 +1,17 @@
+package br.com.youtubemanager.core;
+
+public class YouTubeManagerException extends RuntimeException {
+
+	public YouTubeManagerException() {
+		super("An unexpected error occurred");
+	}
+
+	public YouTubeManagerException(String message) {
+		super(message);
+	}
+
+	public YouTubeManagerException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
